accd48b11d6deecb52fdb132860e240caaca9af6
kao126 <76607249+kao126@users.noreply.github.com>
2022-06-07 16:26:48 +0000

    [Add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  # 顧客用
+  # URL: /customers/...
+  devise_for :customers, controllers: {
+    registrations: "public/devise/registrations",
+    sessions: 'public/devise/sessions',
+    passwords: 'public/devise/passwords'
+  }
+
+  # 管理者用
+  # URL: /admin/sign_in ...
+  devise_for :admins, controllers: {
+    registrations: "admin/devise/registrations",
+    sessions: "admin/devise/sessions",
+    passwords: 'admin/devise/passwords'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
