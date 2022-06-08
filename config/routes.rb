ef969ad491868f1f8d6bf719abee4bd23d5fffa7
kao126 <76607249+kao126@users.noreply.github.com>
2022-06-08 15:53:12 +0000

    [Update]header

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   scope module: :public do
     root to: 'homes#top'
     get 'homes/about'
-    get '/customers/:id/my_page' => "customers#show"
+    get '/customers/:id/my_page' => "customers#show", as: "customer_mypage"
     get '/customers/unsubscribe' => 'customers#unsubscribe'
     patch '/customers/withdraw' => 'customers#withdraw'
 
