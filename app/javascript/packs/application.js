69adb7bcdc754888a7faf95111c180d79dc4a840
kao126 <76607249+kao126@users.noreply.github.com>
2022-06-10 16:54:36 +0000

    [Update]customer jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,24 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
+import "./jquery.jpostal.js"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+// jpostalの記述
+$(document).on('turbolinks:load', function () {
+  $('#customer_postal_code').jpostal({
+    // 取得する郵便番号のテキストボックスをidで指定
+    postcode : ['#customer_postal_code'],
+    address : {
+      // 都道府県: %3 / 市区町村: %4 / 町域: %5 / 番地: %6 / 名称: %7
+      "#customer_prefecture_code" : "%3",
+      "#customer_address_city" : "%4",
+      "#customer_address_street" : "%5%6%7"
+    }
+  });
+});
+
